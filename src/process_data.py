442044428ec21ba0c6ccd2e406e36c9fde15cdc1
MICOMYIZA LIONNEL <lionnel.micomyiza@estiam.com>
2024-03-27 14:30:30 +0100

    RASPUTIN 3

--- src/process_data.py
+++ src/process_data.py
@@ -4,51 +4,53 @@ import os
 import glob
 from pathlib import Path
 import json
- 
+
 col_date: str = "date_heure"
 col_donnees: str = "consommation"
 cols: List[str] = [col_date, col_donnees]
 fic_export_data: str = "data/interim/data.csv"
- 
- 
+
+
 def load_data():
     list_fic: list[str] = [Path(e) for e in glob.glob("data/raw/*json")]
     list_df: list[pd.DataFrame] = []
     for p in list_fic:
-        # list_df.append(pd.read_json(p))
         with open(p, "r") as f:
             dict_data: dict = json.load(f)
             df: pd.DataFrame = pd.DataFrame.from_dict(dict_data.get("results"))
             list_df.append(df)
- 
+
     df: pd.DataFrame = pd.concat(list_df, ignore_index=True)
     return df
- 
- 
+
+
 def format_data(df: pd.DataFrame):
-    # typage
     df[col_date] = pd.to_datetime(df[col_date])
-    # ordre
     df = df.sort_values(col_date)
-    # filtrage colonnes
     df = df[cols]
-    # dédoublonnage
     df = df.drop_duplicates()
     return df
- 
- 
+
+
 def export_data(df: pd.DataFrame):
     os.makedirs("data/interim/", exist_ok=True)
     df.to_csv(fic_export_data, index=False)
- 
- 
+
+
 def main_process():
     df: pd.DataFrame = load_data()
     df = format_data(df)
     export_data(df)
- 
- 
+
+
+def calculate_average_daily_consumption(df: pd.DataFrame):
+    daily_consumption = df.groupby(df[col_date].dt.date)[col_donnees].sum()
+    average_daily_consumption = daily_consumption.mean()
+    return average_daily_consumption
+
+
 if __name__ == "__main__":
- 
-    # data_file: str = "data/raw/eco2mix-regional-tr.csv"
     main_process()
+    df = pd.read_csv(fic_export_data, parse_dates=[col_date])
+    average_daily_consumption = calculate_average_daily_consumption(df)
+    print("Average Daily Consumption:", average_daily_consumption)
