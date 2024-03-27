b65540f5ff0ec4050985554540a8bbc6ed179479
MICOMYIZA LIONNEL <lionnel.micomyiza@estiam.com>
2024-03-27 14:03:24 +0100

    init 25

--- src/process_data.py
+++ src/process_data.py
@@ -1,5 +1,3 @@
-# process_data.py
-
 import pandas as pd
 from typing import List
 import os
@@ -7,7 +5,7 @@ import glob
 from pathlib import Path
 import json
 
-col_date: str = "date_time"  # Update the column name if necessary
+col_date: str = "date_heure"
 col_donnees: str = "consommation"
 cols: List[str] = [col_date, col_donnees]
 fic_export_data: str = "data/interim/data.csv"
@@ -17,16 +15,13 @@ def load_data():
     list_fic: list[str] = [Path(e) for e in glob.glob("data/raw/*json")]
     list_df: list[pd.DataFrame] = []
     for p in list_fic:
+        # list_df.append(pd.read_json(p))
         with open(p, "r") as f:
             dict_data: dict = json.load(f)
             df: pd.DataFrame = pd.DataFrame.from_dict(dict_data.get("results"))
             list_df.append(df)
 
     df: pd.DataFrame = pd.concat(list_df, ignore_index=True)
-    
-    # Print out column names to verify
-    print("Column names:", df.columns)
-    
     return df
 
 
@@ -47,16 +42,6 @@ def export_data(df: pd.DataFrame):
     df.to_csv(fic_export_data, index=False)
 
 
-def calculate_average_consumption_per_hour(df: pd.DataFrame):
-    # Extract hour from the date column
-    df['Hour'] = df[col_date].dt.hour
-    
-    # Group by hour and calculate the mean consumption for each hour
-    average_consumption_per_hour = df.groupby('Hour')[col_donnees].mean()
-    
-    return average_consumption_per_hour
-
-
 def main_process():
     df: pd.DataFrame = load_data()
     df = format_data(df)
@@ -64,4 +49,6 @@ def main_process():
 
 
 if __name__ == "__main__":
+
+    # data_file: str = "data/raw/eco2mix-regional-tr.csv"
     main_process()
